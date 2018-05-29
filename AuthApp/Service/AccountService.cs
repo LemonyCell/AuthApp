43c62ff75898c23f6b68a642f028475b33939dc0
Vitya_80100 <misterstop7@gmail.com>
2018-05-30 02:09:47 +0300

    SQL db connected, validation last

--- AuthApp/Service/AccountService.cs
+++ AuthApp/Service/AccountService.cs
@@ -6,38 +6,78 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MySql.Data.MySqlClient;
 using AuthApp.Domain;
+using System.Windows.Forms;
 
 namespace AuthApp.Service
 {
     public class AccountService
     {
+        private MySqlConnection conn;
+        private string connString;
+
         public ServiceResult<ApplicationUser> Register(RegisterBindingModel model)
         {
             var serviceResult = new ServiceResult<ApplicationUser>();
             //check
-            foreach (var user in DATA.ApplicationUsers)
+            try
             {
-                if (user.Login == model.Login)
+                SetConn();
+                conn.Open();
+
+                if (LoginExist(model.Login))
                 {
                     serviceResult.Error.ErrorCode = 403;
                     serviceResult.Error.ErrorDescription = "User is exist";
-                    return serviceResult;
                 }
+
+                string values = "\"" + Guid.NewGuid().ToString() + "\"," +
+                                "\"" + model.FirstName + "\"," +
+                                "\"" + model.LastName + "\"," +
+                                "\"" + model.Login + "\"," +
+                                "\"" + model.Password + "\"," +
+                                "\"" + model.Email + "\"," +
+                                "\"" + model.PhoneNumber + "\"";
+                var query = "INSERT INTO `Users`(`Id`, `FirstName`, `LastName`, `Login`, `Password`, `Email`, `PhoneNumber`) " +
+                        "VALUES (" + values + ")";
+
+                var cmd = new MySqlCommand(query, conn);
+                var dataReader = cmd.ExecuteReader();
+
+                serviceResult.Success = true;
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                //MessageBox.Show(ex.Message);
+                serviceResult.Error.ErrorCode = 100;
+                serviceResult.Error.ErrorDescription = ex.Message;
+                serviceResult.Success = false;
             }
 
-            var applicationUser = new ApplicationUser();
-            applicationUser.FirstName = model.FirstName;
-            applicationUser.LastName = model.LastName;
-            applicationUser.Login = model.Login;
-            applicationUser.Password = model.Password;
-            applicationUser.PhoneNumber = model.PhoneNumber;
-            applicationUser.Email = model.Email;
-            applicationUser.Id = Guid.NewGuid().ToString();
+            //foreach (var user in DATA.ApplicationUsers)
+            //{
+            //    if (user.Login == model.Login)
+            //    {
+            //        serviceResult.Error.ErrorCode = 403;
+            //        serviceResult.Error.ErrorDescription = "User is exist";
+            //        return serviceResult;
+            //    }
+            //}
 
-            DATA.AddUser(applicationUser);
+            //var applicationUser = new ApplicationUser();
+            //applicationUser.FirstName = model.FirstName;
+            //applicationUser.LastName = model.LastName;
+            //applicationUser.Login = model.Login;
+            //applicationUser.Password = model.Password;
+            //applicationUser.PhoneNumber = model.PhoneNumber;
+            //applicationUser.Email = model.Email;
+            //applicationUser.Id = Guid.NewGuid().ToString();
 
-            serviceResult.Success = true;
+            //DATA.AddUser(applicationUser);
+            
+            conn.Close();
+
             return serviceResult;
         }
 
@@ -45,15 +85,19 @@ namespace AuthApp.Service
         {
             var serviceResult = new ServiceResult<ApplicationUser>();
             //check
-            foreach (var user in DATA.ApplicationUsers)
+
+            try
             {
-                if (user.Login == model.Login)
+                SetConn();
+                conn.Open();
+
+                if (LoginExist(model.Login))
                 {
-                    if (user.Password == model.Password)
+                    if (PasswordIsСorrect(model))
                     {
                         var instanse = Singleton.getInstance();
-                        instanse.User = user;
-                        serviceResult.Result = user;
+                        instanse.User = GetUser(model);
+                        serviceResult.Result = instanse.User;
                         serviceResult.Success = true;
                     }
                     else
@@ -62,8 +106,104 @@ namespace AuthApp.Service
                         serviceResult.Error.ErrorDescription = "Wrong password";
                     }
                 }
+
             }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                //MessageBox.Show(ex.Message);
+                serviceResult.Error.ErrorCode = 100;
+                serviceResult.Error.ErrorDescription = ex.Message;
+                serviceResult.Success = false;
+            }
+
+            //foreach (var user in DATA.ApplicationUsers)
+            //{
+            //    if (user.Login == model.Login)
+            //    {
+            //        if (user.Password == model.Password)
+            //        {
+            //            var instanse = Singleton.getInstance();
+            //            instanse.User = user;
+            //            serviceResult.Result = user;
+            //            serviceResult.Success = true;
+            //        }
+            //        else
+            //        {
+            //            serviceResult.Error.ErrorCode = 405;
+            //            serviceResult.Error.ErrorDescription = "Wrong password";
+            //        }
+            //    }
+            //}
+            conn.Close();
+
             return serviceResult;
         }
+        // true if user exist
+        bool LoginExist(string login)
+        {
+            bool isLogin;
+            string loginStr = "\"" + login + "\"";
+            string query = "SELECT * FROM `Users` WHERE EXISTS ( SELECT * FROM `Users` WHERE Login = " + loginStr + " )";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+            isLogin = dataReader.HasRows;
+            dataReader.Close();
+
+            return isLogin;
+        }
+
+        bool PasswordIsСorrect(AuthBindingModel model)
+        {
+            bool passIsCorr = false;
+            string login = "\"" + model.Login + "\"";
+            string query = "SELECT * FROM `Users` WHERE Login = " + login;
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+            var read = dataReader.Read();
+
+            if (dataReader["Password"] + "" == model.Password)
+            {
+                passIsCorr = true;
+            }
+            dataReader.Close();
+
+            return passIsCorr;
+        }
+
+
+        void SetConn()
+        {
+            if (conn == null)
+            {
+                conn = new MySqlConnection();
+                connString = "SERVER= db4free.net;PORT=3306;DATABASE=users_auth_app;UID=auth_app;PASSWORD=AuthApp123;";
+                conn.ConnectionString = connString;
+            }
+        }
+
+        ApplicationUser GetUser(AuthBindingModel model)
+        {
+            var user = new ApplicationUser();
+
+            string login = "\"" + model.Login + "\"";
+            string query = "SELECT * FROM `Users` WHERE Login = " + login;
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+            var read = dataReader.Read();
+
+            user.Id = dataReader["Id"] + "";
+            user.FirstName = dataReader["FirstName"] + "";
+            user.LastName = dataReader["LastName"] + "";
+            user.Login = dataReader["Login"] + "";
+            user.Password = dataReader["Password"] + "";
+            user.PhoneNumber = dataReader["PhoneNumber"] + "";
+            user.Email = dataReader["Email"] + "";
+
+            dataReader.Close();
+
+            return user;
+        }
+
     }
 }
